421f425bbd9d8b188fbe82c2dadbfdfe89bb928d
JongWoo Park <jjongwoo.dev@gmail.com>
2020-10-12 02:05:59 +0900

    프로젝트-환경설정: JPA와 DB 설정, 동작확인(1)

---
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    // Springboot가 이 어노테이션이 있으면 스프링 컨테이너에 올려서 주입을 시켜줍니다.
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class, id);
+    }
+}
