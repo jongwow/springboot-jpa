1f465c63287436a4b4306c6f9ba3ff1b0bc8249b
JongWoo Park <jjongwoo.dev@gmail.com>
2020-12-20 22:05:28 +0900

    웹 계층 - 상품 취소

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -8,6 +8,7 @@ import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -63,8 +64,7 @@ public class OrderService {
         order.cancel(); // 여기서 update문을 날리지 않는 것이 jpa의 강점
     }
 
-    // 검색
-    //    public List<Order> findOrders(OrderSearch orderSearch) {
-    //        return orderRepository.findAll(orderSearch);
-    //    }
+    public List<Order> findOrders(OrderSearch orderSearch) {
+        return orderRepository.findAllByString(orderSearch);
+    }
 }
