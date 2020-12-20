2fd49493e95626db338fd464657326d35960c2c5
JongWoo Park <jjongwoo.dev@gmail.com>
2020-12-20 14:02:09 +0900

    주문 도메인 개발 - 주문상품,주문 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -67,4 +67,49 @@ public class Order {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
+
+    //=== 생성 메서드===//
+    // 복잡한 생성은 생성 메소드로 하는게 좋다.
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //=== 비즈니스 로직===//
+
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.CAMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : this.orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //=== 조회 로직 ===//
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        return this.orderItems.stream()
+                .mapToInt(OrderItem::getTotalPrice).sum();
+        // 람다를 활용해 더 깔끔하다.
+        /*
+        int totalPrice = 0;
+        for (OrderItem orderItem : this.orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;*/
+    }
 }
