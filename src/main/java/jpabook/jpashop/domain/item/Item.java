2c79fb53959bcb687959901bfa7eb8e50c572d1c
JongWoo Park <jjongwoo.dev@gmail.com>
2020-12-20 13:37:40 +0900

    상품 도메인 개발 - 상품 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,24 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //===비즈니스 로직== Entity안에서 해결할 수 있는 것들은 Entity 안에서 하도록.
+    // Data를 갖고 있는 것에서 data의 값을 변경하는 비즈니스 로직이 있는 것이 객체지향적
+    /**
+     * Stock 증가
+     * */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     *  stcok 감소
+     * */
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
