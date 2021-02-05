2a6a5b8de35816a4d015174e4b540528c5a860d2
JongWoo Park <jjongwoo.dev@gmail.com>
2021-02-06 03:07:25 +0900

    간단한 주문조회 v1-2

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -26,6 +26,10 @@ public class OrderSimpleApiController {
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1(){
         List<Order> all = orderRepository.findAllByString(new OrderSearch()); // 이렇게 OrderSearch를 넘기면 모든걸 가져올것임
+        for (Order order : all) {
+            order.getMember().getName(); // LAZY가 강제 초기화 - aqueryㅇ를 보내 가져오면 됨.
+            order.getDelivery().getAddress();
+        }
         return all;
     }
 }
