9aef63f65c5768878ceab762d467934b3cd020a9
JongWoo Park <jjongwoo.dev@gmail.com>
2021-02-06 23:44:43 +0900

    간단한 주문조회 v2-1

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,22 +1,26 @@
 package jpabook.jpashop.api;
 
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * X to One(ManyToOne, OneToOne)
  * Order
  * Order -> Member
  * Order -> Delivery
- *
- * */
+ */
 @RestController
 @RequiredArgsConstructor
 public class OrderSimpleApiController {
@@ -24,12 +28,40 @@ public class OrderSimpleApiController {
     private final OrderRepository orderRepository;
 
     @GetMapping("/api/v1/simple-orders")
-    public List<Order> ordersV1(){
+    public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAllByString(new OrderSearch()); // 이렇게 OrderSearch를 넘기면 모든걸 가져올것임
         for (Order order : all) {
-            order.getMember().getName(); // LAZY가 강제 초기화 - aqueryㅇ를 보내 가져오면 됨.
+            order.getMember().getName(); // LAZY가 강제 초기화 - query를 보내 가져오면 됨.
             order.getDelivery().getAddress();
         }
         return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() { // 반환은 이렇게 하기보단 그냥 result로 반환하는게 낫다.
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order o) {
+            /* dto가 entity를 파라미터로 받는건 괜찮음.*/
+            orderId = o.getId();
+            name = o.getMember().getName();
+            orderDate = o.getOrderDate();
+            orderStatus = o.getStatus();
+            address = o.getDelivery().getAddress();
+        }
     }
 }
